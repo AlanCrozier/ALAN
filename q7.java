c7bc22f2511f0c9d7d48f973c441232417346efa
Alan <alancrozierjhs@gmail.com>
2025-08-15 16:06:53 +0530

    all

---
+++ q7.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class q7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double l = sc.nextDouble();
+        double w = sc.nextDouble();
+        double perimeter = 2 * (l + w);
+        System.out.println(perimeter);
+    }
+}
